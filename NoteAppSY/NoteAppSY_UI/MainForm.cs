f63847a583e0b986f2c22521c81927426b1146ef
MElias94 <laraxmishin@gmail.com>
2024-11-27 13:56:49 +0300

    Упрощен код. Добавлен класс для UnitTests

--- NoteAppSY/NoteAppSY_UI/MainForm.cs
+++ NoteAppSY/NoteAppSY_UI/MainForm.cs
@@ -38,6 +38,11 @@ namespace NoteAppSY_UI
         }
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            ExportNotes();
+        }
+
+        public void ExportNotes()
+        {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
             saveFileDialog.Title = "Сохранить заметки";
@@ -65,8 +70,12 @@ namespace NoteAppSY_UI
                 }
             }
         }
-
+        
         private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImportNotes();
+        }
+        public void ImportNotes()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
@@ -101,7 +110,7 @@ namespace NoteAppSY_UI
 
                             _noteList.Notes.Add(note);
                             UpdateNotesListBox();
-                            categoryComboBox_SelectedIndexChanged(sender, e);
+                            CategoryChange();
                             ClearTextForms();
 
                         }
@@ -123,52 +132,12 @@ namespace NoteAppSY_UI
 
         private void addNoteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Note newNote = new Note();
-            newNote.Id = Guid.NewGuid();
-            newNote.Name = "Новая заметка";
-            newNote.Category = "Other";
-            newNote.LastUpdate = DateTime.Now;
-            newNote.CreateTime = DateTime.Now;
-            var edit = new EditForm(); //Создаем форму 
-            edit.Note = newNote; //Передаем форме данные
-            edit.ShowDialog(); //Отображаем форму для редактирования
-            if (edit.DialogResult == DialogResult.OK) //При нажатии ок на форме Edit создаем новую заметку
-            {
-                var updatedNote = edit.Note;
-                _noteList.Notes.Add(updatedNote);
-                UpdateNotesListBox();
-                categoryComboBox_SelectedIndexChanged(sender, e);
-                //notesListBox.SelectedIndex = 0;
-            }
+            AddNote();
         }
 
         private void editNoteToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (notesListBox.SelectedIndex == -1)
-            {
-                // Если ничего не выбрано, выводим предупреждение
-                MessageBox.Show("Select note before editing");
-                return;
-            }
-            //Получаем текущую выбранную заметку
-            var selectedIndex = notesListBox.SelectedIndex;
-            var selectedNote = _noteList.FilteredNotes[selectedIndex];
-            var edit = new EditForm(); //Создаем форму 
-            edit.Note = selectedNote; //Передаем форме данные
-            edit.ShowDialog(); //Отображаем форму для редактирования
-            if (edit.DialogResult == DialogResult.OK) //При нажатии ок на форме Edit обновляем список
-            {
-                var updatedNote = edit.Note; //Забираем измененные данные
-                //Осталось удалить старые данные по выбранному индексу
-                // и заменить их на обновленные
-                int originalIndex = _noteList.Notes.IndexOf(selectedNote);
-                _noteList.Notes.RemoveAt(originalIndex);
-                _noteList.Notes.Insert(originalIndex, updatedNote);
-                UpdateNotesListBox();
-                categoryComboBox_SelectedIndexChanged(sender, e);
-                notesListBox.SelectedIndex = 0;
-            }
-            else notesListBox.SelectedIndex = selectedIndex;
+            EditNote();
         }
         /// <summary>
         /// Код для заполнения формы заметками
@@ -193,6 +162,72 @@ namespace NoteAppSY_UI
             }
         }
         private void removeNoteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveNote();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var about = new AboutForm(); //Создаем форму About
+            about.ShowDialog();
+        }
+
+        // Действие при смене категории
+        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CategoryChange();
+        }
+
+        public void AddNote()
+        {
+            Note newNote = new Note();
+            newNote.Id = Guid.NewGuid();
+            newNote.Name = "Новая заметка";
+            newNote.Category = "Other";
+            newNote.LastUpdate = DateTime.Now;
+            newNote.CreateTime = DateTime.Now;
+            var edit = new EditForm(); //Создаем форму 
+            edit.Note = newNote; //Передаем форме данные
+            edit.ShowDialog(); //Отображаем форму для редактирования
+            if (edit.DialogResult == DialogResult.OK) //При нажатии ок на форме Edit создаем новую заметку
+            {
+                var updatedNote = edit.Note;
+                _noteList.Notes.Add(updatedNote);
+                UpdateNotesListBox();
+                CategoryChange();
+                notesListBox.SelectedIndex = 0;
+            }
+        }
+
+        public void EditNote()
+        {
+            if (notesListBox.SelectedIndex == -1)
+            {
+                // Если ничего не выбрано, выводим предупреждение
+                MessageBox.Show("Select note before editing");
+                return;
+            }
+            //Получаем текущую выбранную заметку
+            var selectedIndex = notesListBox.SelectedIndex;
+            var selectedNote = _noteList.FilteredNotes[selectedIndex];
+            var edit = new EditForm(); //Создаем форму 
+            edit.Note = selectedNote; //Передаем форме данные
+            edit.ShowDialog(); //Отображаем форму для редактирования
+            if (edit.DialogResult == DialogResult.OK) //При нажатии ок на форме Edit обновляем список
+            {
+                var updatedNote = edit.Note; //Забираем измененные данные
+                //Осталось удалить старые данные по выбранному индексу
+                // и заменить их на обновленные
+                int originalIndex = _noteList.Notes.IndexOf(selectedNote);
+                _noteList.Notes.RemoveAt(originalIndex);
+                _noteList.Notes.Insert(originalIndex, updatedNote);
+                UpdateNotesListBox();
+                CategoryChange();
+                notesListBox.SelectedIndex = 0;
+            }
+            else notesListBox.SelectedIndex = selectedIndex;
+        }
+        public void RemoveNote()
         {
             // Проверяем, что элемент выбран
             if (notesListBox.SelectedIndex != -1)
@@ -242,14 +277,7 @@ namespace NoteAppSY_UI
             }
         }
 
-        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var about = new AboutForm(); //Создаем форму About
-            about.ShowDialog();
-        }
-
-        // Действие при смене категории
-        private void categoryComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        public void CategoryChange()
         {
             if (notesCategory.SelectedItem != null)
             {
@@ -312,7 +340,7 @@ namespace NoteAppSY_UI
 
         private void addPictureBox_Click(object sender, EventArgs e)
         {
-            addNoteToolStripMenuItem_Click(sender, e);
+            AddNote();
         }
         private void addPictureBox_MouseHover(object sender, EventArgs e)
         {
@@ -323,7 +351,7 @@ namespace NoteAppSY_UI
         }
         private void editPictureBox_Click(object sender, EventArgs e)
         {
-            editNoteToolStripMenuItem_Click(sender, e);
+            EditNote();
         }
         private void editPictureBox_MouseHover(object sender, EventArgs e)
         {
@@ -334,7 +362,7 @@ namespace NoteAppSY_UI
         }
         private void removePictureBox_Click(object sender, EventArgs e)
         {
-            removeNoteToolStripMenuItem_Click(sender, e);
+            RemoveNote();
         }
         private void removePictureBox_MouseHover(object sender, EventArgs e)
         {
